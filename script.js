de6c4a23cc64cccc36c7216598fcbf36dd2d467b
GustaKowai <gusta.kowai@gmail.com>
2023-10-25 23:10:44 -0300

    add: bg, function to change charMood

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var container = document.querySelector(".text")
 
 var speeds = {
   slow: 120,
-  normal: 70,
+  normal: 80,
   fast: 40
 }
 
@@ -12,12 +12,12 @@ var textLines = [
   { string: "O que você faz aqui?", speed: speeds.fast },
   { string: "Não lembro de ter te chamado.", speed: speeds.slow, classes:["red"]}
 ]
-
+//Linhas de texto de falas
 var letras = [];
 
 textLines.forEach((line,index)=> {
   if (index < textLines.length-1){
-    line.string += " ";
+    line.string += "  ";
   }
   line.string.split("").forEach(letra=>{
     var span = document.createElement("span");
@@ -31,6 +31,23 @@ textLines.forEach((line,index)=> {
     })
   })
 })
+//faz a ação de escrever
+function changeSpriteChar(mood){
+  console.log(mood)
+  if(mood == "mad"){
+    activeMood = "img/personagens/char1/5.gif"
+  }
+  if(mood == "happy"){
+    activeMood = "img/personagens/char1/2.gif"
+  }
+  if(mood == "normal"){
+    activeMood = "img/personagens/char1/1.gif"
+  }
+  console.log(mood)
+ 
+  document.getElementById("activeChar").src = activeMood;  
+}
+//muda o humor do personagem
 
 function revealOneCharacter(list){
   var next = list.splice(0,1)[0];
@@ -43,7 +60,28 @@ function revealOneCharacter(list){
     setTimeout(function(){
       revealOneCharacter(list);
     },delay)
-  } 
+  }
+  //checa a mudança de humor:
+  let spanClassList = next.span.classList;
+  let hasRed = spanClassList.contains("red");
+  let hasGreen = spanClassList.contains("green");
+  if (hasGreen){
+      changeSpriteChar("happy")
+      console.log("Feliz")
+  }
+ 
+  if (hasRed){
+      changeSpriteChar("mad");
+      console.log("Puto")
+  }
+  if (!hasGreen && !hasRed){
+    changeSpriteChar("normal")
+    console.log("normal")
+  }
+ 
 }
 
+
 revealOneCharacter(letras);
+
+
