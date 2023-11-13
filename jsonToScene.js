40ffbbf72c2bde1268a65163038dd7660ecf803c
GustaKowai <gusta.kowai@gmail.com>
2023-11-13 02:06:25 -0300

    add: sistema de escolhas e katarina sprites

--- jsonToScene.js
+++ jsonToScene.js
@@ -1,8 +1,16 @@
-import {textToBox,removeLastText} from "./textBoxScript.js"
+import {textToBox,removeLastText,textToChoices} from "./textBoxScript.js"
 //,changeSpriteChar,revealOneCharacter
 const nxtBtn = document.getElementById('nextText');
+const choose1 = document.getElementById('escolha1');
+const choose2 = document.getElementById('escolha2');
+const choose3 = document.getElementById('escolha3');
+const choose4 = document.getElementById('escolha4');
 
 var add = 0;
+var paraCena1 = "";
+var paraCena2 = "";
+var paraCena3 = "";
+var paraCena4 = "";
 var speeds = {
   slow: 120,
   normal: 80,
@@ -10,24 +18,49 @@ var speeds = {
 }
 var letras = [];
 async function readJson(path){
+    console.log(path)
     fetch(path)
     .then((response) => response.json())
     .then((json) => json.cena)
     //.then((teste)=> console.log(teste))
-    .then((value)=> textToBox(value))
+    .then(function (value){
+        textToChoices(value);
+        paraCena1 = value.escolhas.escolha1.paraCena;
+        paraCena2 = value.escolhas.escolha2.paraCena;
+        paraCena3 = value.escolhas.escolha3.paraCena;
+        paraCena4 = value.escolhas.escolha4.paraCena;
+        textToBox(value);
+        
+    })
     //.then(console.log(letras))
     //.then(revealOneCharacter(letras))
 }
 function clicado(){
-    console.log("cliquei")
-};
+    console.log("cliquei");
+}
 //readJson('cena1.json')
 nxtBtn.addEventListener('click',function(){
     if (add > 0){removeLastText()}
     readJson('cena'+add+'.json');
-    add+=1 
-    
+    add+=1;
 });
-
+function escolhido(num) {
+      document.getElementById("escolha1").disabled = true;
+      document.getElementById("escolha2").disabled = true;
+      document.getElementById("escolha3").disabled = true;
+      document.getElementById("escolha4").disabled = true;
+      num == 1? readJson('cena'+paraCena1+'.json'):console.log("nao foi para a 1");
+      num == 2? readJson('cena'+paraCena2+'.json'):console.log("nao foi para a 2");
+      num == 3? readJson('cena'+paraCena3+'.json'):console.log("nao foi para a 3");
+      num == 4? readJson('cena'+paraCena4+'.json'):console.log("nao foi para a 4");
+    }
+choose1.addEventListener('click',function(){
+    removeLastText();
+    escolhido(1);
+});
+choose2.addEventListener('click',function(){
+    removeLastText();
+    escolhido(2);
+});
 //revealOneCharacter(letras);
 
--- textBoxScript.js
+++ textBoxScript.js
@@ -1,22 +1,110 @@
 var container = document.querySelector(".text")
+var escolha1 = document.getElementById("escolha1")
+var escolha2 = document.getElementById("escolha2")
+var escolha3 = document.getElementById("escolha3")
+var escolha4 = document.getElementById("escolha4")
 var speeds = {
   slow: 120,
   normal: 80,
   fast: 40
 }
-
-function removeLastText(){
-    var lastText = document.querySelectorAll('.revealed');
-    lastText.forEach(revealed=>{revealed.remove();});
+//remove o texto do balão antes de colocar novo texto
+function removeLastText() {
+  var lastText = document.querySelectorAll('.revealed');
+  lastText.forEach(revealed => { revealed.remove(); });
+}
+//Coloca os textos dentro das escolhas
+function textToChoices(value) {
+  let nChoices = value.dialogos.numeroEscolhas
+  var paraCena1 = value.escolhas.escolha1.paraCena;
+  var textChoice1 = value.escolhas.escolha1.texto;
+  textChoice1.forEach((line, index) => {
+    if (index < textChoice1.length - 1) {
+      line.string += "  ";
+    }
+    line.string.split("").forEach(letra => {
+      var span = document.createElement("span");
+      span.textContent = letra;
+      escolha1.appendChild(span);
+      letras.push({
+        span: span,
+        classes: line.classes || [],
+      })
+    })
+  })
+  //console.log(paraCena1);
+  if (nChoices > 1) {
+    var paraCena2 = value.escolhas.escolha2.paraCena;
+    var textChoice2 = value.escolhas.escolha2.texto;
+    textChoice2.forEach((line, index) => {
+    if (index < textChoice2.length - 1) {
+      line.string += "  ";
+    }
+    line.string.split("").forEach(letra => {
+      var span = document.createElement("span");
+      span.textContent = letra;
+      escolha2.appendChild(span);
+      letras.push({
+        span: span,
+        classes: line.classes || [],
+      })
+    })
+  })
+   // console.log(paraCena2);
+  }
+  if (nChoices > 2) {
+    var paraCena3 = value.escolhas.escolha3.paraCena;
+    var textChoice3 = value.escolhas.escolha3.texto;
+     textChoice3.forEach((line, index) => {
+    if (index < textChoice3.length - 1) {
+      line.string += "  ";
+    }
+    line.string.split("").forEach(letra => {
+      var span = document.createElement("span");
+      span.textContent = letra;
+      escolha3.appendChild(span);
+      letras.push({
+        span: span,
+        classes: line.classes || [],
+      })
+    })
+  })
+    //console.log(paraCena3);
+  }
+  if (nChoices > 3) {
+    var paraCena4 = value.escolhas.escolha4.paraCena;
+    var textChoice4 = value.escolhas.escolha4.texto;
+     textChoice4.forEach((line, index) => {
+    if (index < textChoice4.length - 1) {
+      line.string += "  ";
+    }
+    line.string.split("").forEach(letra => {
+      var span = document.createElement("span");
+      span.textContent = letra;
+      escolha4.appendChild(span);
+      letras.push({
+        span: span,
+        classes: line.classes || [],
+      })
+    })
+  })
+   // console.log(paraCena4);
+  }
+  //console.log(value);
 }
 //Linhas de texto de falas
 var letras = [];
 function textToBox(cena) {
+  //pega todas as variáveis necessárias
   var activeChar = cena.dialogos.personagem
   var textLines = cena.dialogos.texto;
   var backgroundImg = cena.background;
-  var backgroundUrl = "url('img/background/"+backgroundImg+".jpg')"
+  var nEscolhas = cena.dialogos.numeroEscolhas
+  var numeroEscolhas = parseInt(nEscolhas, 10);
+  //Muda o cenário
+  var backgroundUrl = "url('img/background/" + backgroundImg + ".jpg')"
   document.body.style.backgroundImage = backgroundUrl;
+  //Passa o texto para a caixa de texto, separando as letras
   textLines.forEach((line, index) => {
     if (index < textLines.length - 1) {
       line.string += "  ";
@@ -34,57 +122,65 @@ function textToBox(cena) {
     })
   })
 
-//textToBox(textLines);
-//textToBox(textLines2);
-//faz a ação de escrever
-function changeSpriteChar(mood) {
-  var activeMood = "0";
-  if (mood == "mad") {
-    activeMood = "img/personagens/"+activeChar+"/5.gif"
+  //textToBox(textLines);
+  //textToBox(textLines2);
+  //faz a ação de escrever
+  function changeSpriteChar(mood) {
+    var activeMood = "0";
+    activeMood = "img/personagens/" + activeChar + "/" + mood + ".gif"
+    //if (mood == "mad") {
+    //    activeMood = "img/personagens/"+activeChar+"/5.gif"
+    //  }
+    //  if (mood == "happy") {
+    //    activeMood = "img/personagens/"+activeChar+"/2.gif"
+    //  }
+    //  if (mood == "normal") {
+    //    activeMood = "img/personagens/"+activeChar+"/1.gif"
+    //  }
+
+    document.getElementById("activeChar").src = activeMood;
   }
-  if (mood == "happy") {
-    activeMood = "img/personagens/"+activeChar+"/2.gif"
-  }
-  if (mood == "normal") {
-    activeMood = "img/personagens/"+activeChar+"/1.gif"
+  //muda o humor do personagem
+
+  function revealOneCharacter(list) {
+    var next = list.splice(0, 1)[0];
+    next.span.classList.add("revealed");
+    next.classes.forEach((c) => {
+      next.span.classList.add(c);
+    });
+    var delay = next.isSpace ? 0 : next.delayAfter;
+    if (list.length > 0) {
+      setTimeout(function () {
+        revealOneCharacter(list);
+        //console.log(delay)
+      }, delay)
+    } else {
+      //revela as escolhas
+      escolha1.disabled = false;
+      escolha2.disabled = false;
+      if (numeroEscolhas > 2) { escolha3.disabled = false; }
+      if (numeroEscolhas > 3) { escolha4.disabled = false; }
+    }
+
+    //checa a mudança de humor:
+    let spanClassList = next.span.classList;
+    let isMad = spanClassList.contains("mad");
+    let isHappy = spanClassList.contains("happy");
+    let isNormal = spanClassList.contains("normal");
+    if (isHappy) {
+      changeSpriteChar("happy");
+    }
+
+    if (isMad) {
+      changeSpriteChar("mad");
+    }
+    if (isNormal) {
+      changeSpriteChar("normal");
+    }
+
   }
 
-  document.getElementById("activeChar").src = activeMood;
+  revealOneCharacter(letras);
 }
-//muda o humor do personagem
-
-function revealOneCharacter(list) {
-  var next = list.splice(0, 1)[0];
-  next.span.classList.add("revealed");
-  next.classes.forEach((c) => {
-    next.span.classList.add(c);
-  });
-  var delay = next.isSpace ? 0 : next.delayAfter;
-  if (list.length > 0) {
-    setTimeout(function () {
-      revealOneCharacter(list);
-      //console.log(delay)
-    }, delay)
-  }
-  //checa a mudança de humor:
-  let spanClassList = next.span.classList;
-  let isMad = spanClassList.contains("mad");
-  let isHappy = spanClassList.contains("happy");
-  let isNormal = spanClassList.contains("normal");
-  if (isHappy) {
-    changeSpriteChar("happy");
-  }
-
-  if (isMad) {
-    changeSpriteChar("mad");
-  }
-  if (isNormal) {
-    changeSpriteChar("normal");
-  }
-
-}
-
-revealOneCharacter(letras);
-}
-export{textToBox,removeLastText};
+export { textToBox, removeLastText, textToChoices };
 //,changeSpriteChar,revealOneCharacter
