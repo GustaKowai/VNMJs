b2f11f8c3baa83540997cba485188d4683b574c0
GustaKowai <gusta.kowai@gmail.com>
2023-11-20 17:11:26 -0300

    Add:cenas

--- jsonToScene.js
+++ jsonToScene.js
@@ -1,66 +1,99 @@
-import {textToBox,removeLastText,textToChoices} from "./textBoxScript.js"
+import { textToBox, removeLastText, textToChoices } from "./textBoxScript.js"
 //,changeSpriteChar,revealOneCharacter
 const nxtBtn = document.getElementById('nextText');
+const startBtn = document.getElementById('start');
 const choose1 = document.getElementById('escolha1');
 const choose2 = document.getElementById('escolha2');
 const choose3 = document.getElementById('escolha3');
 const choose4 = document.getElementById('escolha4');
 
-var add = 0;
+var add = "0";
 var paraCena1 = "";
 var paraCena2 = "";
 var paraCena3 = "";
 var paraCena4 = "";
+var nBalao = 0
+var atual = ""
 var speeds = {
-  slow: 120,
-  normal: 80,
-  fast: 40
+    slow: 120,
+    normal: 80,
+    fast: 40
 }
+var balao = 0
 var letras = [];
-async function readJson(path){
+async function readJson(path, balao) {
     console.log(path)
+    removeLastText();
+    atual = path
+    console.log(balao)
     fetch(path)
-    .then((response) => response.json())
-    .then((json) => json.cena)
-    //.then((teste)=> console.log(teste))
-    .then(function (value){
-        textToChoices(value);
-        paraCena1 = value.escolhas.escolha1.paraCena;
-        paraCena2 = value.escolhas.escolha2.paraCena;
-        paraCena3 = value.escolhas.escolha3.paraCena;
-        paraCena4 = value.escolhas.escolha4.paraCena;
-        textToBox(value);
-        
-    })
+        .then((response) => response.json())
+        .then((json) => json.cena)
+        //.then((teste)=> console.log(teste))
+        .then(function (value) {
+            nBalao = value.dialogos.numeroBaloes;
+            if (balao < nBalao) {
+                textToBox(value, balao);
+                textToChoices(value);
+            }
+            paraCena1 = value.escolhas.escolha1.paraCena;
+            paraCena2 = value.escolhas.escolha2.paraCena;
+            paraCena3 = value.escolhas.escolha3.paraCena;
+            paraCena4 = value.escolhas.escolha4.paraCena;
+            balao < nBalao - 1 ? console.log("Ainda tem balao") : console.log("acabaram os baloes")
+
+
+        })
     //.then(console.log(letras))
     //.then(revealOneCharacter(letras))
 }
-function clicado(){
+function clicado() {
     console.log("cliquei");
 }
 //readJson('cena1.json')
-nxtBtn.addEventListener('click',function(){
-    if (add > 0){removeLastText()}
-    readJson('cena'+add+'.json');
-    add+=1;
+startBtn.addEventListener('click', function () {
+    if (add > 0) { removeLastText() }
+    console.log("cliquei aqui")
+    readJson('cena' + add + '.json', 0);
+    add += 1;
+    document.getElementById("start").disabled = true;
+    nxtBtn.disabled = false;
+});
+nxtBtn.addEventListener('click', function () {
+    removeLastText();
+    balao += 1
+    console.log(add)
+    readJson(atual, balao);
 });
 function escolhido(num) {
-      document.getElementById("escolha1").disabled = true;
-      document.getElementById("escolha2").disabled = true;
-      document.getElementById("escolha3").disabled = true;
-      document.getElementById("escolha4").disabled = true;
-      num == 1? readJson('cena'+paraCena1+'.json'):console.log("nao foi para a 1");
-      num == 2? readJson('cena'+paraCena2+'.json'):console.log("nao foi para a 2");
-      num == 3? readJson('cena'+paraCena3+'.json'):console.log("nao foi para a 3");
-      num == 4? readJson('cena'+paraCena4+'.json'):console.log("nao foi para a 4");
-    }
-choose1.addEventListener('click',function(){
+    document.getElementById("escolha1").disabled = true;
+    document.getElementById("escolha2").disabled = true;
+    document.getElementById("escolha3").disabled = true;
+    document.getElementById("escolha4").disabled = true;
+    num == 1 ? readJson('cena' + paraCena1 + '.json', 0) : console.log("nao foi para a 1");
+    num == 2 ? readJson('cena' + paraCena2 + '.json', 0) : console.log("nao foi para a 2");
+    num == 3 ? readJson('cena' + paraCena3 + '.json', 0) : console.log("nao foi para a 3");
+    num == 4 ? readJson('cena' + paraCena4 + '.json', 0) : console.log("nao foi para a 4");
+}
+choose1.addEventListener('click', function () {
     removeLastText();
+    balao = 0;
     escolhido(1);
 });
-choose2.addEventListener('click',function(){
+choose2.addEventListener('click', function () {
     removeLastText();
+    balao = 0;
     escolhido(2);
+});
+choose3.addEventListener('click', function () {
+    removeLastText();
+    balao = 0;
+    escolhido(3);
+});
+choose4.addEventListener('click', function () {
+    removeLastText();
+    balao = 0;
+    escolhido(4);
 });
 //revealOneCharacter(letras);
 
--- textBoxScript.js
+++ textBoxScript.js
@@ -8,13 +8,16 @@ var speeds = {
   normal: 80,
   fast: 40
 }
+var acabouBalao = false
 //remove o texto do balão antes de colocar novo texto
 function removeLastText() {
   var lastText = document.querySelectorAll('.revealed');
   lastText.forEach(revealed => { revealed.remove(); });
+  console.log("Texto")
 }
 //Coloca os textos dentro das escolhas
 function textToChoices(value) {
+  console.log("Escreveu as opções")
   let nChoices = value.dialogos.numeroEscolhas
   var paraCena1 = value.escolhas.escolha1.paraCena;
   var textChoice1 = value.escolhas.escolha1.texto;
@@ -25,6 +28,7 @@ function textToChoices(value) {
     line.string.split("").forEach(letra => {
       var span = document.createElement("span");
       span.textContent = letra;
+      span.classList.add("revealed");
       escolha1.appendChild(span);
       letras.push({
         span: span,
@@ -37,71 +41,78 @@ function textToChoices(value) {
     var paraCena2 = value.escolhas.escolha2.paraCena;
     var textChoice2 = value.escolhas.escolha2.texto;
     textChoice2.forEach((line, index) => {
-    if (index < textChoice2.length - 1) {
-      line.string += "  ";
-    }
-    line.string.split("").forEach(letra => {
-      var span = document.createElement("span");
-      span.textContent = letra;
-      escolha2.appendChild(span);
-      letras.push({
-        span: span,
-        classes: line.classes || [],
+      if (index < textChoice2.length - 1) {
+        line.string += "  ";
+      }
+      line.string.split("").forEach(letra => {
+        var span = document.createElement("span");
+        span.textContent = letra;
+        span.classList.add("revealed");
+        escolha2.appendChild(span);
+        letras.push({
+          span: span,
+          classes: line.classes || [],
+        })
       })
     })
-  })
-   // console.log(paraCena2);
+    // console.log(paraCena2);
   }
   if (nChoices > 2) {
     var paraCena3 = value.escolhas.escolha3.paraCena;
     var textChoice3 = value.escolhas.escolha3.texto;
-     textChoice3.forEach((line, index) => {
-    if (index < textChoice3.length - 1) {
-      line.string += "  ";
-    }
-    line.string.split("").forEach(letra => {
-      var span = document.createElement("span");
-      span.textContent = letra;
-      escolha3.appendChild(span);
-      letras.push({
-        span: span,
-        classes: line.classes || [],
+    textChoice3.forEach((line, index) => {
+      if (index < textChoice3.length - 1) {
+        line.string += "  ";
+      }
+      line.string.split("").forEach(letra => {
+        var span = document.createElement("span");
+        span.textContent = letra;
+        span.classList.add("revealed");
+        escolha3.appendChild(span);
+        letras.push({
+          span: span,
+          classes: line.classes || [],
+        })
       })
     })
-  })
     //console.log(paraCena3);
   }
   if (nChoices > 3) {
     var paraCena4 = value.escolhas.escolha4.paraCena;
     var textChoice4 = value.escolhas.escolha4.texto;
-     textChoice4.forEach((line, index) => {
-    if (index < textChoice4.length - 1) {
-      line.string += "  ";
-    }
-    line.string.split("").forEach(letra => {
-      var span = document.createElement("span");
-      span.textContent = letra;
-      escolha4.appendChild(span);
-      letras.push({
-        span: span,
-        classes: line.classes || [],
+    textChoice4.forEach((line, index) => {
+      if (index < textChoice4.length - 1) {
+        line.string += "  ";
+      }
+      line.string.split("").forEach(letra => {
+        var span = document.createElement("span");
+        span.textContent = letra;
+        span.classList.add("revealed");
+        escolha4.appendChild(span);
+        letras.push({
+          span: span,
+          classes: line.classes || [],
+        })
       })
     })
-  })
-   // console.log(paraCena4);
+    // console.log(paraCena4);
   }
   //console.log(value);
 }
 //Linhas de texto de falas
 var letras = [];
-function textToBox(cena) {
+function textToBox(cena, balao) {
   //pega todas as variáveis necessárias
   var activeChar = cena.dialogos.personagem
   var textLines = cena.dialogos.texto;
   var backgroundImg = cena.background;
   var nEscolhas = cena.dialogos.numeroEscolhas
+  var nBalao = cena.dialogos.numeroBaloes;
   var numeroEscolhas = parseInt(nEscolhas, 10);
-  var balao = 0;
+  document.getElementById('nextText').disabled = true
+  nBalao -= 1
+  balao < nBalao ? acabouBalao = false : acabouBalao = true
+  console.log("Acabou o balao " + acabouBalao, balao, nBalao)
   //Muda o cenário
   var backgroundUrl = "url('img/background/" + backgroundImg + ".jpg')"
   document.body.style.backgroundImage = backgroundUrl;
@@ -125,63 +136,129 @@ function textToBox(cena) {
 
   //textToBox(textLines);
   //textToBox(textLines2);
-  //faz a ação de escrever
+
+
+  //muda o humor do personagem
   function changeSpriteChar(mood) {
     var activeMood = "0";
     activeMood = "img/personagens/" + activeChar + "/" + mood + ".gif"
-    //if (mood == "mad") {
-    //    activeMood = "img/personagens/"+activeChar+"/5.gif"
-    //  }
-    //  if (mood == "happy") {
-    //    activeMood = "img/personagens/"+activeChar+"/2.gif"
-    //  }
-    //  if (mood == "normal") {
-    //    activeMood = "img/personagens/"+activeChar+"/1.gif"
-    //  }
-
     document.getElementById("activeChar").src = activeMood;
   }
-  //muda o humor do personagem
+  function changePlayer(player) {
+    if (player == "orange" || player == "blue") {
+      container.classList.add("player-" + player);
+      escolha1.classList.add("player-" + player);
+      escolha2.classList.add("player-" + player);
+      escolha3.classList.add("player-" + player);
+      escolha4.classList.add("player-" + player);
+    }
+    if (player == "change"){
+      container.classList.toggle("player-orange");
+      escolha1.classList.toggle("player-orange");
+      escolha2.classList.toggle("player-orange");
+      escolha3.classList.toggle("player-orange");
+      escolha4.classList.toggle("player-orange");
+      container.classList.toggle("player-blue");
+      escolha1.classList.toggle("player-blue");
+      escolha2.classList.toggle("player-blue");
+      escolha3.classList.toggle("player-blue");
+      escolha4.classList.toggle("player-blue");
+    }
+  }
 
-  function revealOneCharacter(list) {
+  //faz a ação de escrever
+  function revealOneCharacter(list, acabouBalao) {
     var next = list.splice(0, 1)[0];
     next.span.classList.add("revealed");
     next.classes.forEach((c) => {
       next.span.classList.add(c);
     });
-    var delay = next.isSpace ? 0 : next.delayAfter;
+    var delay = next.isSpace ? 0 : next.delayAfter - 20;
     if (list.length > 0) {
       setTimeout(function () {
-        revealOneCharacter(list);
+        revealOneCharacter(list, acabouBalao);
         //console.log(delay)
       }, delay)
     } else {
-      //revela as escolhas
-      escolha1.disabled = false;
-      escolha2.disabled = false;
-      if (numeroEscolhas > 2) { escolha3.disabled = false; }
-      if (numeroEscolhas > 3) { escolha4.disabled = false; }
+      console.log(acabouBalao); document.getElementById('nextText').disabled = false;
+      if (acabouBalao) {
+        //revela as escolhas
+        escolha1.disabled = false;
+        if (numeroEscolhas > 1) { escolha2.disabled = false; }
+        if (numeroEscolhas > 2) { escolha3.disabled = false; }
+        if (numeroEscolhas > 3) { escolha4.disabled = false; }
+      }
     }
 
     //checa a mudança de humor:
     let spanClassList = next.span.classList;
-    let isMad = spanClassList.contains("mad");
-    let isHappy = spanClassList.contains("happy");
-    let isNormal = spanClassList.contains("normal");
+    let isMad = spanClassList.contains("brava");
+    let isHappy = spanClassList.contains("feliz");
+    let isNormal = spanClassList.contains("default");
+    let isSerio = spanClassList.contains("serio");
+    let isSurpresa = spanClassList.contains("serio");
+    let isTriste = spanClassList.contains("triste");
+    let isIrritada = spanClassList.contains("irritada");
+    let isMeh = spanClassList.contains("meh");
+    let isMuitoFeliz = spanClassList.contains("muitofeliz");
+    let isSmirk = spanClassList.contains("smirk");
+    let isWithKat = spanClassList.contains("katarina");
+    let isWithAle = spanClassList.contains("alessandra");
+    let isRecepcionista = spanClassList.contains("Recepcionista");
+    let trocaPlayer = spanClassList.contains("trocaPlayer")
+    let isOrange = spanClassList.contains("isOrange")
+    let isBlue = spanClassList.contains("isBlue")
     if (isHappy) {
-      changeSpriteChar("happy");
+      changeSpriteChar("feliz");
     }
-
+    if (isSurpresa) {
+      changeSpriteChar("surpresa");
+    }
     if (isMad) {
-      changeSpriteChar("mad");
+      changeSpriteChar("brava");
     }
     if (isNormal) {
-      changeSpriteChar("normal");
+      changeSpriteChar("default");
     }
-
+    if (isSerio) {
+      changeSpriteChar("serio");
+    }
+    if (isTriste) {
+      changeSpriteChar("triste");
+    }
+    if (isIrritada) {
+      changeSpriteChar("irritada");
+    }
+    if (isMeh) {
+      changeSpriteChar("meh");
+    }
+    if (isMuitoFeliz) {
+      changeSpriteChar("muitofeliz");
+    }
+    if (isSmirk) {
+      changeSpriteChar("smirk");
+    }
+    if (isWithKat) {
+      changeSpriteChar("withKat")
+    }
+    if (isWithAle) {
+      changeSpriteChar("withAle")
+    }
+    if (isRecepcionista) {
+      changeSpriteChar("Recepcionista")
+    }
+    if (trocaPlayer) {
+      changePlayer("change")
+    }
+    if (isBlue) {
+      changePlayer("blue")
+    }
+    if (isOrange) {
+      changePlayer("orange")
+    }
   }
 
-  revealOneCharacter(letras);
+  revealOneCharacter(letras, acabouBalao);
 }
 export { textToBox, removeLastText, textToChoices };
 //,changeSpriteChar,revealOneCharacter
